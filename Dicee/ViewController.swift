4e901fdba93d323ef458de9cd79aa44cce067229
Giovani Lopez <glopez_07@me.com>
2019-02-05 19:58:38 -0600

    Initial Commit

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Gio Lopez on 2/5/19.
+//  Copyright © 2019 Giovani Lopez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
